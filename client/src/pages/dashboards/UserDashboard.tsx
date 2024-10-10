d43d39fb0be731b1bd503606f39f8559da2d068a
coder-mizan01 <p.mizan2441@gmail.com>
2024-10-10 15:04:48 +0600

    update vercel config

--- client/src/pages/dashboards/UserDashboard.tsx
+++ client/src/pages/dashboards/UserDashboard.tsx
@@ -3,6 +3,17 @@
 import { useAppSelector , useAppDispatch } from "../../app/hook";
 import { logout } from "../../features/authSlice";
 
+//css
+import "../../css/UserDashboard.css"
+import { Link } from "react-router-dom";
+
+//icons
+import { FaCartShopping , FaHeart, FaUser, FaLocationDot, FaMoneyBill,  } from "react-icons/fa6";
+import { FaSave } from "react-icons/fa";
+import { GiTwoCoins } from "react-icons/gi";
+
+
+
 const UserDashboard = () => {
     const dispatch = useAppDispatch();
     const {user} = useAppSelector((state)=>state.auth);
@@ -16,11 +27,64 @@ const UserDashboard = () => {
 
   return (
     <>
-     <div className="text-center mt-5">
-        <img src={user?.image} alt="" />
-        <p>name : {user?.name}</p>
-        <p>email : {user?.email}</p>
-        <button onClick={handleLogout}>Logout</button>
+    
+     <div id="user-dashboard">
+         <div className="user-dashboard-inner">
+
+
+            <div className="user-header">
+              <div className="left">
+               <figure className="m-0">{user && <img src={user?.image} />}</figure>
+              <div className="name">
+                  <p>Hello</p>
+                  <p>{user?.name}</p>
+              </div>
+              </div>
+              <div className="right">
+                <button onClick={handleLogout}>Logout</button>
+              </div>
+            
+            </div>
+
+           <div className="user-menus">
+              <div className="orders"><Link to={`/`}>
+              <span className=""><FaCartShopping /></span>
+              <span>orders</span>
+              </Link></div>
+
+              <div className="wishlist"><Link to={`/`}>
+              <span className=""><FaHeart /></span>
+              <span>orders</span>
+              </Link></div>
+
+              <div className="edit-profile"><Link to={`/`}>
+              <span className=""><FaUser  /></span>
+              <span>orders</span>
+              </Link></div>
+
+              <div className="address"><Link to={`/`}>
+              <span className=""><FaLocationDot /></span>
+              <span>orders</span>
+              </Link></div>
+
+              <div className="points"><Link to={`/`}>
+              <span className=""><GiTwoCoins /></span>
+              <span>orders</span>
+              </Link></div>
+
+              <div className="transaction"><Link to={`/`}>
+              <span className=""><FaMoneyBill   /></span>
+              <span>orders</span>
+              </Link></div>
+
+              <div className="buy-later"><Link to={`/`}>
+              <span className=""><FaSave /></span>
+              <span>orders</span>
+              </Link></div>
+              
+           </div>
+
+         </div>
      </div>
     </>
   )
